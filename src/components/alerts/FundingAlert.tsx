19ded346529bd6279ef8d75a9ea4fece805dec9c
binarybaron <86064887+binarybaron@users.noreply.github.com>
2024-11-01 10:36:55 +0100

    feat: Add text about CCS proposal

--- src/components/alerts/FundingAlert.tsx
+++ src/components/alerts/FundingAlert.tsx
@@ -1,23 +1,29 @@
-import { Alert, AlertTitle } from "@material-ui/lab";
-import { TextField } from "@material-ui/core";
+import { Alert, AlertTitle, Typography } from '@mui/material';
+import { TextField } from "@mui/material";
 
 export default function FundingAlert() {
   return (
     <Alert severity="info">
-      <AlertTitle>We need your support</AlertTitle>
-      This project is still under active development. Please visit the COMIT
-      team's Github repository if you are a developer and would like to help.
-      You can also help us fund our operations by directly donating to:
+      <AlertTitle>Thank you for your support!</AlertTitle>
+      <Typography>
+      We have received funding (through CCS) to work on the project full-time for the next 6 months! 
+      Believe me when I say we do not take this for granted. We care deeply about making sure Monero 
+      remains accessible to everyone. Over the coming months, we'll be releasing a rewrite of the GUI 
+      with native rust bindings and support for iOS and Android. Stay tuned!
+      <br/>
+      <br/>
+      If you'd still like to support our ongoing work, you can donate to the following addresses:
+      </Typography>
       <TextField
+        variant="standard"
         label="Monero Donation Address"
         fullWidth
-        value="49LEH26DJGuCyr8xzRAzWPUryzp7bpccC7Hie1DiwyfJEyUKvMFAethRLybDYrFdU1eHaMkKQpUPebY4WT3cSjEvThmpjPa"
-      />
+        value="49LEH26DJGuCyr8xzRAzWPUryzp7bpccC7Hie1DiwyfJEyUKvMFAethRLybDYrFdU1eHaMkKQpUPebY4WT3cSjEvThmpjPa" />
       <TextField
+        variant="standard"
         label="Bitcoin Donation Address"
         fullWidth
-        value="bc1qz6lfs0r206396a9psunmkfpqh7sdf2zh3e7tnf"
-      />
+        value="bc1qz6lfs0r206396a9psunmkfpqh7sdf2zh3e7tnf" />
     </Alert>
   );
 }
